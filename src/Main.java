e4e4e7e27e492bfbe61142bbf7a272a78de1010c
vkray <vitalikkrainyuk@yandex.ru>
2023-07-01 11:40:16 +0300

    hw1.  Работа с исключениями.  Решение задачи

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        String login = "java_skypro_go";
+        String password = "D_1hWiKjjP_9";
+        String confirmPassword = "D_1hWiKjjP_9";
 
+        try {
+            UserValidator.validateUser(login, password, confirmPassword);
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
     }
 }
